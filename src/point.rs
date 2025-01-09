8458cf8f820bf5b14f11e846d545932e28fd4098
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-08 20:32:10 -0500

    Doc Comments

--- src/point.rs
+++ src/point.rs
@@ -4,6 +4,29 @@ pub mod glam;
 pub mod nalgebra;
 pub mod tuple;
 
+/// A point that can be used for Raycasting.
+/** # Examples
+```
+#[derive(Debug, Clone)]
+struct Vec2 {
+	x: f32,
+	y: f32
+}
+
+impl Point for Vec2 {
+	fn x(&self) -> f32 {
+		self.x
+	}
+	fn y(&self) -> f32 {
+		self.y
+	}
+	fn edit(&mut self, x: f32, y: f32) {
+		self.x = x;
+		self.y = y;
+	}
+}
+```
+ */
 pub trait Point: DynClone + core::fmt::Debug {
 	fn x(&self) -> f32;
 	fn y(&self) -> f32;
